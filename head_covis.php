2f76864b2ec9e90b7c9370db2538a5ce3d74736a
pkraker <pkraker@openknowledgemaps.org>
2020-04-08 19:20:51 +0200

    Update CoVis head

--- head_covis.php
+++ head_covis.php
@@ -1,6 +1,17 @@
 <?php
 
 if($EXTERNAL_COMPONENTS) {
+    
+    $default_labels = array(
+        "title" => "CoVis"
+        , "app-name" => "CoVis"
+        , "description" => ""
+        , "url" => (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"
+        , "twitter-type" => "summary"
+        , "twitter-image" => ""
+        , "fb-image" => ""
+    );
+    
     include $EXTERNAL_COMPONENTS_PATH . "head_components/meta_tags.php";
 }
 
