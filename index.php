0a97b179ec553364361272f17c4c4f61adc6dceb
tanteuschi <maxi@openknowledgemaps.org>
2020-05-04 19:37:48 +0200

    other footer and header color for knowledge-map.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ include 'config.php';
         <link type="text/css" rel="stylesheet" href="./main.css">
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-        <script type="text/javascript" src="./menu.js "></script>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="./menu.js "></script>
     </head>
 
     <body class="about generalpage">
