3e4fd19f9a51fa68a4e252c2d49c6309f9de9815
pkraker <pkraker@openknowledgemaps.org>
2020-06-12 11:14:12 +0200

    Add debug flag to config example

--- config.example.php
+++ config.example.php
@@ -3,6 +3,7 @@ $HEADSTART_PATH = "../../";
 $EXTERNAL_COMPONENTS = false;
 $EXTERNAL_COMPONENTS_PATH = "../components/";
 $SHEET_ID = "";
+$DEBUG = false;
 $PIWIK_ENABLED = false;
 $GA_ENABLED = false;
 $GA_CODE = "XX-00000000-0";
