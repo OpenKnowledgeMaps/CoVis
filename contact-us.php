b7a924b159ae45378e76e445f82676bc614832df
tanteuschi <maxi@openknowledgemaps.org>
2020-06-18 13:14:21 +0200

    update of contact page

--- contact-us.php
+++ contact-us.php
@@ -29,23 +29,26 @@ include 'config.php';
 
                 <h1 class="page-headline">Contact us</h1>
 
-                <h4 class="question">We invite subject-matter experts to contribute to CoVis</h4>
-
+                <h4 class="question">For subject-matter experts who want to contribute to CoVis</h4>
+                
+                <p class="paragraph-style">You can contribute to CoVis in a number of ways:</p>
+                
                 <ul class="paragraph-style">
                     <li>To propose research resources or provide feedback on CoVis, please <a href="">fill out this short form</a>.</li>
-                    <li>If you would like to create your own collections and interpretations of research findings, <a target="_blank" href="https://refigure.org/signup/">sign up as a ReFigure user here</a>. Once you have signed up, you can find a tutorial of <a target="_blank" href="https://www.youtube.com/watch?v=FxGLRB6y0tc">how to create ReFigure collections here</a>. After you have created your coronavirus related ReFigure, please send us an email at <a href="mailto:contactrefigure@gmail.com">contactrefigure@gmail.com</a> to begin a discussion with us.
+                    <li>If you would like to create your own collections and interpretations of research findings around key issues, such as whether a drug is effective, we suggest creating a ReFigure. A ReFigure is a user-generated collection of figures from different scientific publications that can be included in the knowledge map. 
+You can <a target="_blank" href="https://refigure.org/signup/">sign up as a ReFigure user here</a>. Once you have signed up, you can find a tutorial of <a target="_blank" href="https://www.youtube.com/watch?v=FxGLRB6y0tc">how to create ReFigure collections here</a>. After you have created your coronavirus related ReFigure, please send us an email at <a href="mailto:contactrefigure@gmail.com">contactrefigure@gmail.com</a> to begin a discussion with us.
                     </li>
                 </ul>
 
                 <p class="paragraph-style">
-                    If you have any more questions on how to contribute please get in touch with team ReFigure at <a href="mailto:contactrefigure@gmail.com">contactrefigure@gmail.com</a>
+                    The curation team is led by immunologists and ReFigure founders Dr. Girija Goyal and Dr. James Akin. If you have any more questions on how to contribute, please get in touch with them at <a href="mailto:contactrefigure@gmail.com">contactrefigure@gmail.com</a>.
                 </p>
                 
 
                 <h4 class="question">All other inquiries</h4>
 
                 <p class="paragraph-style">
-                    Please get in touch with team Open Knowledge Maps at <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>
+                    Please get in touch with team Open Knowledge Maps at <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>.
                 </p>
 
             </div>
