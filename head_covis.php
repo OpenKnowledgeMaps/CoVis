6f54ef9413369d02f2d6019b74c8115403315f18
Jan Konstant <456545@mail.muni.cz>
2021-10-06 15:55:39 +0200

    added font-awesome fonts and style

--- head_covis.php
+++ head_covis.php
@@ -44,3 +44,4 @@ if($EXTERNAL_COMPONENTS) {
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="./js/menu.js "></script>
+<link type="text/css" rel="stylesheet" href="./css/font-awesome.min.css">
